80d8adc8be2755ca5a4c1613b5091b2dc9c97bc8
rnqhqaltjs <rnqhqaltjs@naver.com>
2022-08-28 23:10:22 +0900

    databinding

--- app/src/main/java/com/example/wharareyouupto2/ui/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/wharareyouupto2/ui/viewmodel/EditViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.wharareyouupto2.ui.viewmodel
 
 import android.app.Application
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
@@ -11,10 +13,11 @@ import com.example.wharareyouupto2.data.repository.MemoRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+
 class EditViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository : MemoRepository
-    private var textCounter = ""
+    private var a : String = ""
 
     init{
 
@@ -37,11 +40,18 @@ class EditViewModel(application: Application) : AndroidViewModel(application) {
 
 //    val textwatcher : ObservableField<String> = ObservableField()
 
-    val titlewatcher = ObservableField("0 / 12")
+    val titlewatcher = ObservableField(a.length.toString() + " / 12")
 
     fun titleTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
         Log.w("tag", "onTextChanged $s")
         titlewatcher.set(s.toString().length.toString() + " / 12")
+        a = before.toString().length.toString()
+    }
+
+    var watcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable) {}
     }
 
     val contentwatcher = ObservableField("0 / 50")
