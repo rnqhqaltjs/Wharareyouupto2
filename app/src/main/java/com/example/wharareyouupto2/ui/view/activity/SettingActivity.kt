657aa6e2d1b43769179f777943f9cdf49d3a271c
rnqhqaltjs <rnqhqaltjs@naver.com>
2022-11-03 00:56:01 +0900

    setting

--- app/src/main/java/com/example/wharareyouupto2/ui/view/activity/SettingActivity.kt
+++ app/src/main/java/com/example/wharareyouupto2/ui/view/activity/SettingActivity.kt
@@ -2,19 +2,59 @@ package com.example.wharareyouupto2.ui.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.wharareyouupto2.databinding.ActivitySettingBinding
+import com.example.wharareyouupto2.util.SettingDataStore
+import com.example.wharareyouupto2.util.UIMode
+import kotlinx.coroutines.launch
 
 class SettingActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivitySettingBinding
+    private lateinit var settingDataStore: SettingDataStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        settingDataStore = SettingDataStore(this)
+        observeUIPreferences()
+        initView()
 
+    }
+
+    private fun observeUIPreferences() {
+        settingDataStore.uiModeFlow.asLiveData().observe(this) { uiMode ->
+            setCheckedMode(uiMode)
+        }
+    }
+
+    private fun setCheckedMode(uiMode: UIMode?) {
+        when (uiMode) {
+            UIMode.LIGHT -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.applyModeSwitch.isChecked = false
+            }
+            UIMode.DARK -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.applyModeSwitch.isChecked = true
+            }
+            else -> {}
+        }
+    }
+
+    private fun initView() {
+        binding.applyModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            lifecycleScope.launch {
+                when (isChecked) {
+                    true -> settingDataStore.setDarkMode(UIMode.DARK)
+                    false -> settingDataStore.setDarkMode(UIMode.LIGHT)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/wharareyouupto2/util/UIMode.kt
@@ -0,0 +1,5 @@
+package com.example.wharareyouupto2.util
+
+enum class UIMode {
+    LIGHT, DARK
+}
